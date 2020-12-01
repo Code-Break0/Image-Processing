34de795db1f8f12184324d0c98679d6a57666ff0
Code-Break0 <41364583+Code-Break0@users.noreply.github.com>
2020-11-30 17:12:06 -0800

    Update Image.cpp

--- src/Image.cpp
+++ src/Image.cpp
@@ -199,3 +199,43 @@ Image& Image::decodeMessage(char* buffer, size_t* messageLength) {
 
 
 
+
+
+
+
+
+
+
+
+	Image& Image::flipX() {
+		uint8_t tmp[4];
+		uint8_t* px1;
+		uint8_t* px2;
+		for(int y = 0;y < h;++y) {
+			for(int x = 0;x < w/2;++x) {
+				px1 = &data[(x + y * w) * channels];
+				px2 = &data[((w - 1 - x) + y * w) * channels];
+
+				memcpy(tmp, px1, channels);
+				memcpy(px1, px2, channels);
+				memcpy(px2, tmp, channels);
+			}
+		}
+		return *this;
+	}
+	Image& Image::flipY() {
+		uint8_t tmp[4];
+		uint8_t* px1;
+		uint8_t* px2;
+		for(int x = 0;x < w;++x) {
+			for(int y = 0;y < h/2;++y) {
+				px1 = &data[(x + y * w) * channels];
+				px2 = &data[(x + (h - 1 - y) * w) * channels];
+
+				memcpy(tmp, px1, channels);
+				memcpy(px1, px2, channels);
+				memcpy(px2, tmp, channels);
+			}
+		}
+		return *this;
+	}

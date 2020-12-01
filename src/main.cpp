d5867259fba9d83865d1b8a341cc2caf1ecdf6f0
Code-Break0 <41364583+Code-Break0@users.noreply.github.com>
2020-11-30 17:13:27 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,55 +4,15 @@
 int main(int argc, char** argv) {
 	Image test("imgs/test1.jpg");
 
+	/*
 	
+		Add your own code here to do some cool stuff with images!
+		https://www.youtube.com/playlist?list=PLG5M8QIx5lkzdGkdYQeeCK__As6sI2tOY
+		
+	*/
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// double ker[] = { -2/9.0, -1/9.0, 0,
-	// 				 -1/9.0,  1/9.0, 1/9.0,
-	// 				 0     ,  1/9.0, 2/9.0 }; //emboss
-	// // double ker[] = { 1/16.0, 2/16.0, 1/16.0,
-	// // 				 2/16.0, 4/16.0, 2/16.0,
-	// // 				 1/16.0, 2/16.0, 1/16.0 }; //gaussian blur
-
-	// Image ct0 = test;
-	// ct0.std_convolve_clamp_to_0(0, 3, 3, ker);
-	// ct0.std_convolve_clamp_to_0(1, 3, 3, ker);
-	// ct0.std_convolve_clamp_to_0(2, 3, 3, ker);
-	// ct0.write("imgs/ct0.png");
-
-	// Image ctb = test;
-	// ctb.std_convolve_clamp_to_border(0, 3, 3, ker);
-	// ctb.std_convolve_clamp_to_border(1, 3, 3, ker);
-	// ctb.std_convolve_clamp_to_border(2, 3, 3, ker);
-	// ctb.write("imgs/ctb.png");
-
-	// Image ctc = test;
-	// ctc.std_convolve_cyclic(0, 3, 3, ker);
-	// ctc.std_convolve_cyclic(1, 3, 3, ker);
-	// ctc.std_convolve_cyclic(2, 3, 3, ker);
-	// ctc.write("imgs/ctc.png");
-
-
-	// Image diff = ctb;
-	// diff.diffmap_scale(ctc);
-	// diff.write("imgs/diff.png");
 
 	return 0;
 }
